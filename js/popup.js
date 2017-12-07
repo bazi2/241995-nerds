781c2f614688beed0964899cf891291f36007400
bazi2 <bazi2@rambler.ru>
2017-12-07 17:20:23 +0300

    Отступ в подвале
    Не могу найти откуда взялся отступ

--- js/popup.js
+++ js/popup.js
@@ -1,49 +1,49 @@
 var link = document.querySelector(".footer-button-form");
           
-      var popup = document.querySelector(".modal-contacts-form");
-      var close = popup.querySelector(".modal-close");
+var popup = document.querySelector(".modal-contacts-form");
+var close = popup.querySelector(".modal-close");
           
-      var form = popup.querySelector("form");
-      var login = popup.querySelector("[name=login]");
-      var email = popup.querySelector("[name=email]");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
 
           
-      var storage = localStorage.getItem("login");
+var storage = localStorage.getItem("login");
           
-      link.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        popup.classList.add("modal-show");
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
             
-        if (storage) {
-          login.value = storage;
-          email.focus();
-        } else {
-          login.focus();
-        }
-      });
+  if (storage) {
+    login.value = storage;
+    email.focus();
+  } else {
+    login.focus();
+  }
+});
           
-      close.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        popup.classList.remove("modal-show");
-        popup.classList.remove("modal-error");
-      });
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
           
-      form.addEventListener("submit", function (evt) {
-        if (!login.value || !email.value) {
-          evt.preventDefault();
-          popup.classList.remove("modal-error");
-          popup.offsetWidth = popup.offsetWidth;
-          popup.classList.add("modal-error");
-        } else {
-          localStorage.setItem("login", login.value);
-        }
-      });
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("login", login.value);
+  }
+});
           
-      window.addEventListener("keydown", function (evt) {
-        if (evt.keyCode === 27) {
-          if (popup.classList.contains("modal-show")) {
-            popup.classList.remove("modal-show");
-            popup.classList.remove("modal-error");
-          }
-        }
-      });
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
